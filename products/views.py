26908927b17ebf601f0214c90fcf30895aba984e
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-10-07 02:25:41 +0530

    created views and set url paths

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+  return HttpResponse('<h1>Welcome to Shopping Website home page<h1>')
+
+def about(request):
+  return HttpResponse('<h1>About Page</h1>')
+
+def contact(request):
+  return HttpResponse('<h1>Contact Page<h1>')
