fea5320de928d5c1c49413f139fc2a47fb6dee28
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-10-07 05:53:36 +0530

    admin user created, poducts+offer registered

--- products/models.py
+++ products/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
   name = models.CharField(max_length=255)
   price = models.FloatField()
   stock = models.IntegerField()
   image = models.CharField(max_length=2500)
 
-class Offers(models.Model):
+class Offer(models.Model):
   code = models.CharField(max_length=16)
   description = models.CharField(max_length=255)
   discount = models.FloatField()
